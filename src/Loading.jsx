7f61ebc106b2f7b570103a9c0d5f16d8dde3970f
boadoryan <75857938+boadoryan@users.noreply.github.com>
2023-11-17 10:50:00 -0800

    .

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -4,8 +4,12 @@ const Loading = () => {
   return (
     <>
       <div className="flex flex-col text-center items-center justify-center align-center mt-36 gap-4 mx-12 ">
-        <img className="h-[25rem]" src="assets/loading.svg" alt="loading image" />
-        <div className="font-bold text-4xl">Welcome to Trendie!</div>
+        <img
+          className="h-[25rem]"
+          src="assets/loading.svg"
+          alt="loading image"
+        />
+        <div className="font-bold text-4xl">Welcome to Shoply!</div>
         <div className="font-medium text-xl md:text-2xl">
           Please give us a second as we load the store.
         </div>
